0a9863d188b3dc5befa1ec82fd01de76cf33fbbc
mamun-devxhub <abdullah-mamun@devxhub.com>
2022-06-19 16:10:45 +0600

    f network

--- src/components/CompDashBoard.vue
+++ src/components/CompDashBoard.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard">
-    <div class="grid grid-cols-4 main-info">
+    <div class="grid grid-cols-4 main-info mb-8">
       <div>
         <p class="text-h6">FHM Price</p>
         <p class="text-h5">$1.07</p>
@@ -26,7 +26,7 @@
         <p class="text-h5">$28,432,324</p>
       </div>
     </div>
-    <div class="grid grid-cols-2">
+    <div class="grid grid-cols-2 gap-8">
       <div class="main-info">
         <p class="text-h6">Total Value Deposited</p>
         <p class="text-h5">$2,378,260</p>
@@ -34,6 +34,53 @@
       <div class="main-info">
         <p class="text-h6">Current network values</p>
         <p class="text-h5">Fantom network</p>
+
+        <div class="space-y-3 last-child">
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">fwsFHM Price</p>
+            <p class="text-h5 text-base">$75.43</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Current Index</p>
+            <p class="text-h5 text-base">73.65</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Epoch</p>
+            <p class="text-h5 text-base">9.5 hours</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Staked FHM</p>
+            <p class="text-h5 text-base">89.72%</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Epoch Rebase</p>
+            <p class="text-h5 text-base">0% / 26.02 sFHM</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 90-Day APY</p>
+            <p class="text-h5 text-base">54%</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 90-Day ROI</p>
+            <p class="text-h5 text-base">11%</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 90-Day Rebase</p>
+            <p class="text-h5 text-base">0.04% / 0.1009 fwsFHM</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 180-Day APY</p>
+            <p class="text-h5 text-base">185%</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 180-Day ROI</p>
+            <p class="text-h5 text-base">68%</p>
+          </div>
+          <div class="flex justify-between items-center border-bottom">
+            <p class="text-h6">Accelerated 180-Day Rebase</p>
+            <p class="text-h5 text-base">0.11% / 23.9866 fwsFHM</p>
+          </div>
+        </div>
       </div>
       <div class="main-info">
         <p class="text-h6">Protocol Owned Liquidity FHM-DAI</p>
@@ -57,5 +104,12 @@ export default {};
   border: 2px solid hsla(0, 0%, 100%, 0.1);
   background: none;
   box-shadow: none;
+}
+.border-bottom {
+  border-bottom: 1px solid hsla(0, 0%, 100%, 0.1);
+  padding-bottom: 5px;
+}
+.last-child div:last-child {
+  border-bottom: 0;
 }
 </style>
